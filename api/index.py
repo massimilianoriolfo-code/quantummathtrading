5a6285bd369a2868fa639660dc46c283299fb22b
massimilianoriolfo-code <massimiliano.riolfo@gmail.com>
2026-04-18 18:14:33 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,25 +14,24 @@ def handler(request):
         return ('', 204, headers)
 
     try:
-        # Recupero ticker
         data = request.get_json(silent=True) or {}
         ticker = data.get('ticker', 'SPY').upper()
         
-        # Sourcing
         stock = yf.Ticker(ticker)
         df = stock.history(period="1d")
         
         if df.empty:
             return (json.dumps({"error": "Ticker non trovato"}), 404, headers)
 
-        current_price = float(df['Close'].iloc[-1])
-        move = current_price * 0.20 * (30/365)**0.5
+        price = float(df['Close'].iloc[-1])
+        move = price * 0.20 * (30/365)**0.5
         
-        return (json.dumps({
-            "price": round(current_price, 2),
-            "high": round(current_price + move, 2),
-            "low": round(current_price - move, 2)
-        }), 200, headers)
+        res = {
+            "price": round(price, 2),
+            "high": round(price + move, 2),
+            "low": round(price - move, 2)
+        }
+        return (json.dumps(res), 200, headers)
 
     except Exception as e:
         return (json.dumps({"error": str(e)}), 500, headers)
